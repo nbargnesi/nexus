52fd76ee2c4e06d76c112c361554ec9ad7ea49fc
Nick Bargnesi <nick@den-4.com>
2014-11-27 00:54:37 -0500

    add assert_env and prompt_env from gosh

--- .gosh.sh
+++ .gosh.sh
@@ -18,3 +18,41 @@ function default {
     fi
 }
 
+# Returns 1 if the environment variable $1 is not set, 0 otherwise.
+# E.g.,
+#    assert_env PATH
+function assert_env {
+    if [ $# -ne 1 ]; then
+        echo "usage: assert_env <variable>"
+        echo "(got: $@)"
+        exit 1
+    fi
+    eval __=\$$1
+    if [ -z "$__" ]; then
+        echo "$1 is not set" 1>&2
+        return 1
+    fi
+    return 0
+}
+
+# Prompts the user to set a variable if it does not have a default value.
+# E.g.,
+#    prompt_env VERSION "VERSION is not set, please set it now: "
+function prompt_env {
+    if [ $# -ne 2 ]; then
+        echo "usage: prompt_env <variable> <prompt>"
+        echo "(got: $@)"
+        exit 1
+    fi
+    eval __=\$$1
+    if [ -z "$__" ]; then
+        read -p "$2" REPLY
+        if [ -z "$REPLY" ]; then
+            echo "noo response" 1>&2
+            return 1
+        fi
+        export $1="$REPLY"
+    fi
+    return 0
+}
+
