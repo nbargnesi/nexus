3affe2892df203ed55005ba7cfcd9691b4d2ec5d
Nick Bargnesi <nick@den-4.com>
2014-11-29 13:28:11 -0500

    lowercase local functions
    Functions starting with a capital are meant for export.

--- main.go
+++ main.go
@@ -41,45 +41,45 @@ const (
 )
 
 func main() {
-	env := Getenv(BCAST_INGRESS_PORT)
-	bcast_ingress_port := AsPort(env)
-	env = Getenv(BCAST_EGRESS_PORT)
-	bcast_egress_port := AsPort(env)
-	env = Getenv(RR1_INGRESS_PORT)
-	rr1_ingress_port := AsPort(env)
-	env = Getenv(RR1_EGRESS_PORT)
-	rr1_egress_port := AsPort(env)
-	env = Getenv(RR2_INGRESS_PORT)
-	rr2_ingress_port := AsPort(env)
-	env = Getenv(RR2_EGRESS_PORT)
-	rr2_egress_port := AsPort(env)
+	env := getenv(BCAST_INGRESS_PORT)
+	bcast_ingress_port := asPort(env)
+	env = getenv(BCAST_EGRESS_PORT)
+	bcast_egress_port := asPort(env)
+	env = getenv(RR1_INGRESS_PORT)
+	rr1_ingress_port := asPort(env)
+	env = getenv(RR1_EGRESS_PORT)
+	rr1_egress_port := asPort(env)
+	env = getenv(RR2_INGRESS_PORT)
+	rr2_ingress_port := asPort(env)
+	env = getenv(RR2_EGRESS_PORT)
+	rr2_egress_port := asPort(env)
 
-	Print("starting")
+	pprint("starting")
 
 	// CREATE EACH SOCKET...
-	sub_ingress := NewSocket(zmq.SUB)
+	sub_ingress := newSocket(zmq.SUB)
 	sub_ingress.SetSubscribe("")
 	defer sub_ingress.Close()
-	pub_egress := NewSocket(zmq.PUB)
+	pub_egress := newSocket(zmq.PUB)
 	pub_egress.SetLinger(1)
 	defer pub_egress.Close()
 
-	rr1_ingress := NewSocket(zmq.ROUTER)
+	rr1_ingress := newSocket(zmq.ROUTER)
 	defer rr1_ingress.Close()
-	rr1_egress := NewSocket(zmq.DEALER)
+	rr1_egress := newSocket(zmq.DEALER)
 	defer rr1_egress.Close()
-	rr2_ingress := NewSocket(zmq.ROUTER)
+	rr2_ingress := newSocket(zmq.ROUTER)
 	defer rr2_ingress.Close()
-	rr2_egress := NewSocket(zmq.DEALER)
+	rr2_egress := newSocket(zmq.DEALER)
 	defer rr2_egress.Close()
 
 	// ... AND BIND
-	Bind(sub_ingress, "tcp", "0.0.0.0", bcast_ingress_port)
-	Bind(pub_egress, "tcp", "0.0.0.0", bcast_egress_port)
-	Bind(rr1_ingress, "tcp", "0.0.0.0", rr1_ingress_port)
-	Bind(rr1_egress, "tcp", "0.0.0.0", rr1_egress_port)
-	Bind(rr2_ingress, "tcp", "0.0.0.0", rr2_ingress_port)
-	Bind(rr2_egress, "tcp", "0.0.0.0", rr2_egress_port)
+	bind(sub_ingress, "tcp", "0.0.0.0", bcast_ingress_port)
+	bind(pub_egress, "tcp", "0.0.0.0", bcast_egress_port)
+	bind(rr1_ingress, "tcp", "0.0.0.0", rr1_ingress_port)
+	bind(rr1_egress, "tcp", "0.0.0.0", rr1_egress_port)
+	bind(rr2_ingress, "tcp", "0.0.0.0", rr2_ingress_port)
+	bind(rr2_egress, "tcp", "0.0.0.0", rr2_egress_port)
 
 	poller := zmq.NewPoller()
 	poller.Add(sub_ingress, zmq.POLLIN)
@@ -88,37 +88,37 @@ func main() {
 	poller.Add(rr1_egress, zmq.POLLIN)
 	poller.Add(rr2_egress, zmq.POLLIN)
 
-	Print("greenline alive")
+	pprint("greenline alive")
 	sigchan := make(chan os.Signal, 1)
 	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
 	go func() {
 		for sig := range sigchan {
 			switch sig {
 			case syscall.SIGTERM:
-				Die("received SIGTERM")
+				die("received SIGTERM")
 			case syscall.SIGINT:
-				Die("received SIGINT")
+				die("received SIGINT")
 			case syscall.SIGQUIT:
-				Die("received SIGQUIT")
+				die("received SIGQUIT")
 			}
 		}
 	}()
 
-	Print("greenline ready")
+	pprint("greenline ready")
 	for {
 		sockets, _ := poller.Poll(-1)
 		for _, socket := range sockets {
 			switch s := socket.Socket; s {
 			case sub_ingress:
-				Print("processing broadcast message")
+				pprint("processing broadcast message")
 				for {
 					msg, err := s.Recv(0)
 					if err != nil {
-						Die("broadcast more: %s", err.Error())
+						die("broadcast more: %s", err.Error())
 					}
 					more, err := s.GetRcvmore()
 					if err != nil {
-						Die("broadcast recv more: %s", err.Error())
+						die("broadcast recv more: %s", err.Error())
 					}
 					if more {
 						pub_egress.Send(msg, zmq.SNDMORE)
@@ -128,15 +128,15 @@ func main() {
 					}
 				}
 			case rr1_ingress:
-				Print("processing rr1 request")
+				pprint("processing rr1 request")
 				for {
 					msg, err := s.Recv(0)
 					if err != nil {
-						Die("rr1 ingress: %s", err.Error())
+						die("rr1 ingress: %s", err.Error())
 					}
 					more, err := s.GetRcvmore()
 					if err != nil {
-						Die("rr1 ingress recv more: %s", err.Error())
+						die("rr1 ingress recv more: %s", err.Error())
 					}
 					if more {
 						rr1_egress.Send(msg, zmq.SNDMORE)
@@ -146,15 +146,15 @@ func main() {
 					}
 				}
 			case rr2_ingress:
-				Print("processing rr2 request")
+				pprint("processing rr2 request")
 				for {
 					msg, err := s.Recv(0)
 					if err != nil {
-						Die("rr2 ingress: %s", err.Error())
+						die("rr2 ingress: %s", err.Error())
 					}
 					more, err := s.GetRcvmore()
 					if err != nil {
-						Die("rr2 ingress recv more: %s", err.Error())
+						die("rr2 ingress recv more: %s", err.Error())
 					}
 					if more {
 						rr2_egress.Send(msg, zmq.SNDMORE)
@@ -164,15 +164,15 @@ func main() {
 					}
 				}
 			case rr1_egress:
-				Print("processing rr1 response")
+				pprint("processing rr1 response")
 				for {
 					msg, err := s.Recv(0)
 					if err != nil {
-						Die("rr1 egress: %s", err.Error())
+						die("rr1 egress: %s", err.Error())
 					}
 					more, err := s.GetRcvmore()
 					if err != nil {
-						Die("rr1 egress recv more: %s", err.Error())
+						die("rr1 egress recv more: %s", err.Error())
 					}
 					if more {
 						rr1_ingress.Send(msg, zmq.SNDMORE)
@@ -182,15 +182,15 @@ func main() {
 					}
 				}
 			case rr2_egress:
-				Print("processing rr2 response")
+				pprint("processing rr2 response")
 				for {
 					msg, err := s.Recv(0)
 					if err != nil {
-						Die("rr2 egress: %s", err.Error())
+						die("rr2 egress: %s", err.Error())
 					}
 					more, err := s.GetRcvmore()
 					if err != nil {
-						Die("rr2 egress recv more: %s", err.Error())
+						die("rr2 egress recv more: %s", err.Error())
 					}
 					if more {
 						rr2_ingress.Send(msg, zmq.SNDMORE)
@@ -204,43 +204,43 @@ func main() {
 	}
 }
 
-func Getenv(env string) string {
+func getenv(env string) string {
 	_env := os.Getenv(env)
 	if len(_env) == 0 {
-		Die("no " + env + " is set")
+		die("no " + env + " is set")
 	}
 	return _env
 }
 
-func AsPort(env string) (port int) {
+func asPort(env string) (port int) {
 	port, err := strconv.Atoi(env)
 	if err != nil {
-		Die("invalid port: %s", env)
+		die("invalid port: %s", env)
 	} else if port < 1 || port > 65535 {
-		Die("invalid port: %s", env)
+		die("invalid port: %s", env)
 	}
 	return
 }
 
-func NewSocket(ztype zmq.Type) (socket *zmq.Socket) {
+func newSocket(ztype zmq.Type) (socket *zmq.Socket) {
 	socket, err := zmq.NewSocket(ztype)
 	if err != nil {
-		Die("failed creating socket type %d: %s", ztype, err.Error())
+		die("failed creating socket type %d: %s", ztype, err.Error())
 	}
 	return
 }
 
-func Bind(socket *zmq.Socket, transport string, address string, port int) {
+func bind(socket *zmq.Socket, transport string, address string, port int) {
 	endpoint := fmt.Sprintf("%s://%s:%d", transport, address, port)
-	Out("Binding socket %d... ", port)
+	out("Binding socket %d... ", port)
 	err := socket.Bind(endpoint)
 	if err != nil {
-		Die("failed binding %s: %s", endpoint, err.Error())
+		die("failed binding %s: %s", endpoint, err.Error())
 	}
 	fmt.Println("done.")
 }
 
-func MakeMsg(msg string, args ...interface{}) string {
+func makeMsg(msg string, args ...interface{}) string {
 	const layout = "%d%02d%02d-%02d-%02d-%02d greenline[%d]: %s"
 	now := time.Now()
 	year := now.Year()
@@ -255,19 +255,19 @@ func MakeMsg(msg string, args ...interface{}) string {
 	return ret
 }
 
-func Print(msg string, args ...interface{}) {
-	msg = MakeMsg(msg, args...)
+func pprint(msg string, args ...interface{}) {
+	msg = makeMsg(msg, args...)
 	fmt.Fprintf(os.Stdout, msg+"\n")
 }
 
-func Out(msg string, args ...interface{}) {
-	msg = MakeMsg(msg, args...)
+func out(msg string, args ...interface{}) {
+	msg = makeMsg(msg, args...)
 	fmt.Fprintf(os.Stdout, msg)
 	os.Stdout.Sync()
 }
 
-func Die(msg string, args ...interface{}) {
-	msg = MakeMsg(msg, args...)
+func die(msg string, args ...interface{}) {
+	msg = makeMsg(msg, args...)
 	fmt.Fprintf(os.Stderr, msg+"\n")
 	os.Exit(1)
 }
